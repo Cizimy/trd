b3def5a27a27266821c99637a6f9d92dd3f442c6
Cizimy <kenichi.4d@gmail.com>
2017-12-28 15:21:43 +0900

    test2

---
+++ mongotest2.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+#from pymongo import MongoClient
+import requests
+
+#client = MongoClient('10.142.0.3', 27017)
+
+#db = client.test_database
+#collection = db.test_collection
+
+r = requests.get("https://api.binance.com/api/v3/ticker/price")
+
+posts = r.json()
+#result1 = collection.insert_many(posts)
+
+print(posts)
+
