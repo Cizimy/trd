b74a4fa61b9f5bb5fe93791516cfd306bc7e0659
Cizimy <kenichi.4d@gmail.com>
2017-12-30 00:30:36 +0900

    w

--- signal.py
+++ signal.py
@@ -1,14 +1,17 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import signal
 import time
+import requests
 
 def task(arg1, arg2):
-    print(time.time())
+    r = requests.get("https://api.binance.com/api/v3/ticker/bookTicker?symbol=BNBBTC")
+    data = r.json()
+    print(data)
 
 signal.signal(signal.SIGALRM, task)
 signal.setitimer(signal.ITIMER_REAL, 0.1, 0.1)
