c5a0f6d20d34ce45e7eed6ce78417e7c0a7c4ec6
Cizimy <kenichi.4d@gmail.com>
2017-12-30 04:14:46 +0900

    a

--- sig.py
+++ sig.py
@@ -11,7 +11,7 @@ def task(arg1, arg2):
     print(time.time())
 
 signal.signal(signal.SIGALRM, task)
-signal.setitimer(signal.ITIMER_REAL, 1, 1)
+signal.setitimer(signal.ITIMER_REAL, 0.1, 0.1)
 
 while True:
     time.sleep(1)
