7b83475eca17d1d8d7044c68335c5f0ad5ca8ba4
Cizimy <kenichi.4d@gmail.com>
2017-12-30 01:13:32 +0900

    a

--- signal.py
+++ signal.py
@@ -15,9 +15,9 @@ db = client.ticker
 collection = db.binance
 
 def task(arg1, arg2):
-    r = requests.get("https://api.binance.com/api/v3/ticker/bookTicker?symbol=BNBBTC")
-    post = r.json()
-    result1 = collection.insert_one(post)
+    r = requests.get("https://api.binance.com/api/v3/ticker/bookTicker")
+    posts = r.json()
+    result1 = collection.insert_many(posts)
 
 signal.signal(signal.SIGALRM, task)
 signal.setitimer(signal.ITIMER_REAL, 1, 1)
